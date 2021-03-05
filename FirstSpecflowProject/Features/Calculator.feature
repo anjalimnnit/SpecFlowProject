35080cb2793463ace32d10657c09590bc5474309
Shah <Anjali.Shah@philips.com>
2021-03-05 18:31:58 +0530

    Add project files.

---
+++ FirstSpecflowProject/Features/Calculator.feature
@@ -0,0 +1,13 @@
+﻿Feature: Calculator
+
+Scenario Outline: Operate two numbers
+	Given I have a Calculator
+	When I apply <operation> on <first> and <second>
+	Then the result should be <result>
+	
+	Examples: 
+	| operation | first | second | result |
+	| add       | 40    | 25     | 65     |
+	| subtract  | 34    | 10     | 24     |
+	| multiply  | 25    | 4      | 100    |
+	| divide    | 50    | 2      | 25    |
---
+++ FirstSpecflowProject/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,47 @@
+﻿using SpecFlowCalculator;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+using TechTalk.SpecFlow.Assist;
+
+
+namespace FirstSpecflowProject.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        private Calculator _calculator;
+        private int _result;
+        [Given(@"I have a Calculator")]
+        public void GivenIHaveACalculator()
+        {
+            _calculator = new Calculator();
+        }
+
+        [When(@"I apply (.*) on (.*) and (.*)")]
+        public void WhenIApplyAddOnAnd(string operation,int first, int second)
+        {
+            _calculator.FirstNumber = first;
+            _calculator.SecondNumber = second;
+            _result = operation switch
+            {
+                "add" => _calculator.Add(),
+                "subtract" => _calculator.Subtract(),
+                "multiply" => _calculator.Multiply(),
+                "divide" => _calculator.Divide(),
+                _ => 0,
+            };
+        }
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+
+
+
+
+
+
+    }
+}
